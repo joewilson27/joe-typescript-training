59f4cc3e95f79bcc8509c77c514aaf53c5ff37e7
Joe Wilson <wilsonmangunsong@gmail.com>
2022-06-25 05:15:48 +0700

    sec6_vid20_to_vid24
    Inheritence Class

---
+++ typescript-training/05-inheritance/ArrayDriver.ts
@@ -0,0 +1,17 @@
+import { Shape } from "./Shape";
+import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
+
+let myShape = new Shape(10, 15);
+let myCircle = new Circle(5, 10, 20);
+let myRectangle = new Rectangle(0, 0, 3, 7);
+
+// declare an array of shapes ... initially empty, Deklarasi array type object Shape. Kurang lebih jadi kayak Polymorphism di Java
+let theShapes: Shape[] = [];
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+for (let tempShape of theShapes) {
+    console.log(tempShape.getInfo());
+}
